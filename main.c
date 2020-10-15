63270e90dbadde8040cebc95bad55fc32281e651
yuuuniii49 <0409yoon@sookmyung.ac.kr>
2020-10-15 15:33:23 +0900

    01

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int flag=1 ;
+	int y;
+	
+	while(flag!=0){
+
+		y=3;
+		flag=0;
+	}
+	y=4;
+	return 0;
+}
