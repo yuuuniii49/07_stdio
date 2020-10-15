a080d9d4d35936e95757d7e5e1d6359dc3037693
yuuuniii49 <0409yoon@sookmyung.ac.kr>
2020-10-15 16:11:20 +0900

    05

--- main.c
+++ main.c
@@ -3,18 +3,21 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void f(void);
-
-int i;
+void sub(void);
 int main(int argc, char *argv[]) {
-	for(i=0;i<5;i++){
-		f();
-	}	
+	int i;
+	for(i=0;i<3;i++)
+	sub();
 	return 0;
 }
-void f(void)
-{
-	for(i=0;i<10;i++)
-		printf("#");
+
+void sub(void)
+{ 
+	int auto_count=0;
+	static int static_count=0;
+	auto_count++;
+	static_count++;
+	printf("auto_count=%d\n",auto_count);
+	printf("static_count=%d\n",static_count);
 }
 
